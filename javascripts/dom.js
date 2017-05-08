a6a27874693753fdb9ba4ab986b0442ab97628ce
Heather Thacker <hhthacker@gmail.com>
2017-05-08 18:02:45 -0500

    card style

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -12,26 +12,26 @@ var movieAPI = ((movie) => {
 
       results.forEach((movie) => {
         if (movie.isSeen === true){
-          seen += `<div class="col-xs-4 card-container">`;
+          seen += `<div class="col-xs-4 card-container jumbotron">`;
           seen += `<span class="glyphicon glyphicon-remove pull-right"></span>`;
-          seen += `<img src="http://forkliftsystems.com.au/wp-content/uploads/2015/04/placeholder-200x200.png">`;
+          seen += `<img src="http://www.seriouseats.com/images/20110325-food-lab-tater-tots-mushy-center.jpg">`;
           seen += `<h3>${movie.Title}</h3>`;
           seen += `<p>${movie.Plot}</p>`;
           seen += `<p>${movie.Actors}`;
           seen += `<p>${movie.Year}</p>`;
-          seen += `<div class="ratingSystem container">`
-          seen += `<h6>Rating</h6>`
-          seen += `<input name="star" type="radio" class="star"/>`
-          seen += `<label class="star-1" for="star-1">1</label>`
-          seen += `<input name="star" type="radio" class="star"/>`
-          seen += `<label class="star-1" for="star-1">2</label>`
-          seen += `<input name="star" type="radio" class="star"/>`
-          seen += `<label class="star-1" for="star-1">3</label>`
-          seen += `<input name="star" type="radio" class="star"/>`
-          seen += `<label class="star-1" for="star-1">4</label>`
-          seen += `<input name="star" type="radio" class="star"/>`
-          seen += `<label class="star-1" for="star-1">5</label>`
-          seen += `</div>`
+          seen += `<div class="ratingSystem container">`;
+          seen += `<h6>Rating</h6>`;
+          seen += `<input name="star" type="radio" class="star"/>`;
+          seen += `<label class="star-1" for="star-1">1</label>`;
+          seen += `<input name="star" type="radio" class="star"/>`;
+          seen += `<label class="star-1" for="star-1">2</label>`;
+          seen += `<input name="star" type="radio" class="star"/>`;
+          seen += `<label class="star-1" for="star-1">3</label>`;
+          seen += `<input name="star" type="radio" class="star"/>`;
+          seen += `<label class="star-1" for="star-1">4</label>`;
+          seen += `<input name="star" type="radio" class="star"/>`;
+          seen += `<label class="star-1" for="star-1">5</label>`;
+          seen += `</div>`;
           seen += `</div>`;
         } else {
           notSeen += `<div class="col-xs-4 card-container">`;
