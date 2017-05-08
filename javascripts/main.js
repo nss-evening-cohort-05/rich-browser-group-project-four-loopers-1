85027ade413f88aa2fc8bb3b0d498f41a78b722c
Mitchell Blom <mitchellblom@gmail.com>
2017-05-08 17:42:56 -0500

    show hide when user logs in fixed

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,48 +1,48 @@
 let apiKeys = {};
 
-	movieAPI.firebaseCredentials().then((keys) => {
-        apiKeys = keys;
-        firebase.initializeApp(apiKeys);
-        // console.log("api keys: ", apiKeys);
-        // movieAPI.writeDom(apiKeys);
-    }).catch((error) => {
-        console.log("key errors", error);
-    });
+movieAPI.firebaseCredentials().then((keys) => {
+    apiKeys = keys;
+    firebase.initializeApp(apiKeys);
+    // console.log("api keys: ", apiKeys);
+    // movieAPI.writeDom(apiKeys);
+}).catch((error) => {
+    console.log("key errors", error);
+});
 
 $(document).ready(function() {
 
-	$("body").keyup(function() {
-		if (window.event.keyCode === 13) {
- 			getMovieFromDb();
-		}
-	 });
-
-  $('#getMovie').click((event) => {
-    let movieTitle = $('#movieSearch').val();
-    let movieActors = event.Actors;
-    let newMovie = {
-        isSeen: false,
-        movie: movieTitle,
-        actors: movieActors
-    };
-    movieAPI.getMovie(movieTitle).then((results) =>{
-      console.log("Movie API results:", results);
-			movieAPI.addMovie(apiKeys, results);						// call post movie to firebase here
-    }).catch((error) => {
-      console.log("getMovie Error", error);
+    $("body").keyup(function() {
+        if (window.event.keyCode === 13) {
+            getMovieFromDb();
+        }
     });
-  		// getMovie();									// maybe don't want to do this
-  });
 
-  let getMovieFromDb = (event) => {
-  	let movieTitle = $('#movieSearch').val();
-	    movieAPI.getMovie(movieTitle).then((results) =>{
-		movieAPI.writeDom(results);
-	      console.log("Movie API results:", results);
-	    }).catch((error) => {
-	      console.log("getMovie Error", error);
-	    });
-  }
+    $('#getMovie').click((event) => {
+        let movieTitle = $('#movieSearch').val();
+        let movieActors = event.Actors;
+        let newMovie = {
+            isSeen: false,
+            movie: movieTitle,
+            actors: movieActors
+        };
+        movieAPI.getMovie(movieTitle).then((results) => {
+            console.log("Movie API results:", results);
+            movieAPI.addMovie(apiKeys, results); 		// call post movie to firebase here
+        }).catch((error) => {
+            console.log("getMovie Error", error);
+        });
+        // getMovie();									// maybe don't want to do this
+    });
+
+    let getMovieFromDb = (event) => {
+        let movieTitle = $('#movieSearch').val();
+        movieAPI.getMovie(movieTitle).then((results) => {
+            movieAPI.writeDom(results);
+            console.log("Movie API results:", results);
+        }).catch((error) => {
+            console.log("getMovie Error", error);
+        });
+    }
 
     $("#loginButton").click(() => {
         let email = $('#inputEmail').val();
@@ -56,9 +56,8 @@ $(document).ready(function() {
         movieAPI.loginUser(user).then((response) => {
             clearLogin();
             $('#login-container').addClass('hide');
-            $('.main-container').removeClass('hide');
-           movieAPI.writeDom(apiKeys);
-
+            $('#movie-container').removeClass('hide');
+            movieAPI.writeDom(apiKeys);
             movieAPI.createLogoutButton(apiKeys);
         }).catch((error) => {
             console.log("error in loginUser: ", error);
@@ -84,7 +83,7 @@ $(document).ready(function() {
                 movieAPI.loginUser(user).then((response) => {
                     clearLogin();
                     $('#login-container').addClass('hide');
-                    $('.main-container').removeClass('hide');
+                    $('#movie-container').removeClass('hide');
                     movieAPI.writeDom(apiKeys);
                     movieAPI.createLogoutButton(apiKeys);
                 }).catch((error) => {
@@ -102,13 +101,13 @@ $(document).ready(function() {
         clearLogin();
         movieAPI.logoutUser();
         $('#login-container').removeClass('hide');
-        $('.main-container').addClass('hide');
+        $('#movie-container').addClass('hide');
         $('#logout-container').html("");
     });
 
     let clearLogin = () => {
-    	$("#inputEmail").val("");
-    	$("#inputPassword").val("");
+        $("#inputEmail").val("");
+        $("#inputPassword").val("");
         $("#inputUsername").val("");
     };
 
